f6b288140fee4bf36ff9deea5e2c3f76a0a2948f
Xuriwork <xuriwork@gmail.com>
2023-06-02 13:44:38 -0400

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-escape */
 
 export const YOUTUBE_VIDEO_URL_REGEX = /^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/|\/embed\/.+$/;
-export const socketURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.REACT_APP_GAE_API_URL;
+export const socketURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : process.env.REACT_APP_RAILWAY_API_URL;
 
 export const convertURLToYoutubeVideoId = (url) => {
     let id = '';
